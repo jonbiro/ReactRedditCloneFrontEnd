6e8dde4c144cf17a6d17e9a231ce06a176c656e3
Jonathan Biro <jonnybrx@gmail.com>
2019-02-12 17:07:56 -0500

    Added Go home button on post page

--- redditclone/src/components/PostListItem.js
+++ redditclone/src/components/PostListItem.js
@@ -9,9 +9,6 @@ export default function PostListItem({ title, postId, votes, upFunc, downFunc })
         <h3>{title}</h3>
       </Link>
         <Voting postId={postId} title={title} votes={votes} upFunc={upFunc} downFunc={downFunc}/>
-      {/*<p>*/}
-          {/*<span role="img" aria-label="up" id="upVote" onClick={ () => upFunc(postId, votes)}> ⬆️ </span>️ {votes} <span role="img" aria-label="down" id="downVote" onClick={ () => downFunc(postId, votes)}> ⬇️ </span>*/}
-      {/*</p>*/}
     </div>
   );
 }
